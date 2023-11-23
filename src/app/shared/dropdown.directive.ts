0e01e22180c327333203c88a5d7130f79a841ff2
aminnegahban73 <amin.negahban1@gmail.com>
2023-11-23 23:49:46 +0330

    Dropdown Clickout to close it

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -5,9 +5,19 @@ import { Directive, HostBinding, HostListener, Input } from '@angular/core';
   exportAs: 'appDropDown',
 })
 export class DropdownDirective {
+  private wasInside = false;
+
   @HostBinding('class.show') isShow: boolean = false;
   constructor() {}
   @HostListener('click') toggleShow() {
     this.isShow = !this.isShow;
+    this.wasInside = true;
+  }
+
+  @HostListener('document:click') clickout() {
+    if (!this.wasInside) {
+      this.isShow = false;
+    }
+    this.wasInside = false;
   }
 }
