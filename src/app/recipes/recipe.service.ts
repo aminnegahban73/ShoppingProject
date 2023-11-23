8c438500a4032a59c2d78d7fa810e6c5683dd5c3
aminnegahban73 <amin.negahban1@gmail.com>
2023-11-24 00:15:03 +0330

    Managing Recipes in Recipe Service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  constructor() {}
+  private recipes: Recipe[] = [
+    new Recipe(
+      'دستور پخت زرشک پلو با مرغ',
+      'برای پخت زرشک‌پلو با مرغ ابتدا مرغ خردشده را با پیاز، زردچوبه و نمک مخلوط می‌‌‌‌کنیم و آن را روی حرارت متوسط به مدت 20 دقیقه تفت می‌‌‌‌دهیم تا مرغ کاملا مزه دار شود.',
+      'https://arseschef.com/wp-content/uploads/2022/06/%D8%B2%D8%B1%D8%B4%DA%A9-%D9%BE%D9%84%D9%88-%D8%A8%D8%A7-%D9%85%D8%B1%D8%BA.png'
+    ),
+    new Recipe(
+      'Recipe Number 2',
+      'This is a test recipe.This is a test recipe.',
+      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'
+    ),
+  ];
+
+  public getRecipes() {
+    return this.recipes.slice();
+  }
+}
